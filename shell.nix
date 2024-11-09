35a79bda24b5d8094ee6d550bae507cff8f9875e
Darkflame72 <leon@darkflame.dev>
2024-11-09 16:03:21 +1300

    feat: move to using nix-shell

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
+pkgs.mkShell {
+  packages = [
+    pkgs.direnv
+    pkgs.age
+    pkgs.flux
+    pkgs.helmfile
+    pkgs.go-task
+    pkgs.sops
+    pkgs.jq
+    pkgs.fluxcd
+    pkgs.python3
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.kubeconform
+    pkgs.kustomize
+    pkgs.talosctl
+    pkgs.cloudflared
+    pkgs.envsubst
+    pkgs.kubectl-cnpg
+    pkgs.yq
+    (builtins.getFlake "github:budimanjojo/talhelper").packages.x86_64-linux.default
+  ];
+}
