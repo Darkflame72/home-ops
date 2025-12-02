7d716eb5ff11dc74cdc19199a3e9b869e4067ab7
Darkflame72 <leon@darkflame.dev>
2025-12-03 00:20:45 +1300

    feat: add initial talos config

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{
+}:
+let
+  pkgs = import (fetchTarball "https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz") { };
+
+in
+pkgs.mkShell {
+  packages = [
+    pkgs.cilium-cli
+    pkgs.cloudflared
+    pkgs.age
+    pkgs.argocd
+    pkgs.sops
+    pkgs.helmfile
+    pkgs.kustomize
+    pkgs.kubectl
+    pkgs.talosctl
+    pkgs.kubernetes-helm
+    pkgs.talhelper
+    pkgs.cmctl
+    pkgs.kubectl-cnpg
+    pkgs.kubectl-rook-ceph
+    pkgs.kubectl-view-secret
+    pkgs.infisical
+  ];
+
+  KUBECONFIG = "${toString ./.}/kubeconfig";
+  SOPS_AGE_KEY_FILE = "${toString ./.}/age.key";
+  TALOSCONFIG = "${toString ./.}/talos/clusterconfig/talosconfig";
+
+}
